be0892fc9bca4b1f9176ba3aa6e562c2d0567c16
matimon nakdook <safemontima@gmail.com>
2023-02-28 04:12:41 +0700

    test cors 2

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'domain-a.com')
+  res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')
   res.header('Access-Control-Allow-Headers','Content-Type, Option, Authorization')
   return next()
