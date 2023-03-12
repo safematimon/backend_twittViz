9e7086a93a0ca5a1d0318902709a7182a1f39238
matimon nakdook <safemontima@gmail.com>
2023-03-13 02:27:05 +0700

    fix database

--- app.js
+++ app.js
@@ -9,17 +9,18 @@ require('dotenv').config();
 
 const http = require('http')
 const path = require('path')
-// const socketIO = require('socket.io')
+// const socketIO = require('socket.io');
+const { disconnect } = require('process');
 const PORT = 4000
 
-// const server = http.createServer(app)
-// const io = socketIO(server)
 const server = http.createServer(app);
 // const io = socketIO(server)
 
-const token = process.env.BEARER_TOKEN;
+// const TOKEN = 'AAAAAAAAAAAAAAAAAAAAAOmfggEAAAAAVUwVxCz0gTPEfEwpN7%2BwKZ5zst8%3DZLkIGJFA89LW0okCZZhEwhMCXm1ZpSyjufJlyaadkUFVblH7ii';
+const TOKEN = 'AAAAAAAAAAAAAAAAAAAAAPqPiwEAAAAAgE5PZHyvxwRbRn0sQ4T4vBY2%2FG8%3DFlYzmplCpHrpGOYGzTYUfvLzEA5k3uaJG9fNgaGoUDux67qNwC'
 const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
-const streamURL = 'https://api.twitter.com/2/tweets/search/stream';
+// const streamURL = 'https://api.twitter.com/2/tweets/search/stream';
+const streamURL = 'https://api.twitter.com/2/tweets/search/stream?tweet.fields=created_at,entities,lang&expansions=author_id';
 
 
 // // mongoose
@@ -45,162 +46,166 @@ app.get('/', async (req, res, next) => {
 
 app.use('/api', require('./routes/api.route'));
 
-// // test zone
 
-// // // app.use((err, req, res, next) => {
-// // //   res.status(err.status || 500);
-// // //   res.send({
-// // //     status: err.status || 500,
-// // //     message: err.message,
-// // //   });
-// // // });
+// // ========================================================  
+// //socket zone
+// let rules = [{ value: 'dog' }]
+// let stream
 
-// // app.listen(4000, () => {
-// //   // api 
-// //   // console.log('running on port 4000');
-// // });
-
-// // ====
-// // The code below sets the bearer token from your environment variables
-// // To set environment variables on macOS or Linux, run the export command below from the terminal:
-// // export BEARER_TOKEN='YOUR-TOKEN'
-// // const http = require('http')
-// // const path = require('path')
-// // const socketIO = require('socket.io')
-// // const PORT = 5000
-
-// // const server = http.createServer(app)
-// // const io = socketIO(server)
-
-// // const token = process.env.BEARER_TOKEN;
-// // const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
-// // const streamURL = 'https://api.twitter.com/2/tweets/search/stream';
-
-// // Edit rules as desired below
-// const rules = [{
-//         'value': 'Blackpink -is:retweet',
-//         // 'tag': 'dog pictures'
+// // Get stream rules
+// async function getRules() {
+//   const response = await needle('get', rulesURL, {
+//     headers: {
+//       Authorization: `Bearer ${TOKEN}`,
 //     },
-// ];
-
-// async function getAllRules() {
-//     const response = await needle('get', rulesURL, {
-//         headers: {
-//             "authorization": `Bearer ${token}`
-//         }
-//     })
-//     if (response.statusCode !== 200) {
-//         console.log("Error:", response.statusMessage, response.statusCode)
-//         throw new Error(response.body);
-//     }
-//     return (response.body);
+//   })
+//   console.log("here?",response.body)
+//   return response.body
 // }
 
-// async function deleteAllRules(rules) {
-//   if (!Array.isArray(rules.data)) {
-//       return null;
-//   }
-//   const ids = rules.data.map(rule => rule.id);
+// // Set stream rules
+// async function setRules() {
 //   const data = {
-//       "delete": {
-//           "ids": ids
-//       }
+//     add: rules,
+//   }
+
+//   const response = await needle('post', rulesURL, data, {
+//     headers: {
+//       'content-type': 'application/json',
+//       Authorization: `Bearer ${TOKEN}`,
+//     },
+//   })
+//   console.log("set",response.body)
+//   return response.body
+// }
+
+// // Delete stream rules
+// async function deleteRules(rules) {
+//   if (!Array.isArray(rules.data)) {
+//     return null
+//   }
+
+//   const ids = rules.data.map((rule) => rule.id)
+
+//   const data = {
+//     delete: {
+//       ids: ids,
+//     },
 //   }
 //   const response = await needle('post', rulesURL, data, {
-//       headers: {
-//           "content-type": "application/json",
-//           "authorization": `Bearer ${token}`
-//       }
+//     headers: {
+//       'content-type': 'application/json',
+//       Authorization: `Bearer ${TOKEN}`,
+//     },
 //   })
-//   if (response.statusCode !== 200) {
-//       throw new Error(response.body);
-//   }
-//   return (response.body);
+
+//   return response.body
 // }
 
-// async function setRules() {
-//     const data = {
-//         "add": rules
-//     }
-//     const response = await needle('post', rulesURL, data, {
-//         headers: {
-//             "content-type": "application/json",
-//             "authorization": `Bearer ${token}`
+// let count =0
+// let re_count=0
+// let hashtag=[]
+
+// function streamTweets(socket) {
+//   stream = needle.get(streamURL, {
+
+//     headers: {
+//       Authorization: `Bearer ${TOKEN}`,
+//     },
+//   })
+//   console.log("starting...")
+
+//   stream.on('data', (data) => {
+//     try {
+//       const json = JSON.parse(data)
+//       socket.emit('tweet', json)
+//       socket.emit('no', count)
+//       // console.log(count,">",json)
+
+//       const regexRetweet = /^RT\s+@/;
+//       let matches = json.data.text.match(regexRetweet);
+//       if (matches) {re_count++;}
+//       socket.emit('re', re_count)
+
+//       if(json.data.entities?.hashtags){
+//         for (let i = 0; i < json.data.entities.hashtags.length; i++) {
+//           let tag = json.data.entities.hashtags[i].tag;
+//           hashtag.push(`#${tag}`);
 //         }
-//     })
-//     if (response.statusCode !== 201) {
-//         throw new Error(response.body);
-//     }
-//     return (response.body);
-// }
-
-// function streamConnect(retryAttempt) {
-//   const stream = needle.get(streamURL, {
-//     // config here
-//     // const stream = needle.get(`${streamURL}?tweet.field`, {
-//   headers: {
-//           "User-Agent": "v2FilterStreamJS",
-//           "Authorization": `Bearer ${token}`
-//       },
-//       timeout: 20000
-//   });
-//   stream.on('data', data => {
-//       try {
-//           const json = JSON.parse(data);
-//           // print data here
-//           console.log(">>>>",json);
-//           retryAttempt = 0;
-//       } catch (e) {
-//           if (data.detail === "This stream is currently at the maximum allowed connection limit.") {
-//               console.log(data.detail)
-//               process.exit(1)
-//           } else {
-//           }
 //       }
-//   }).on('err', error => {
-//       if (error.code !== 'ECONNRESET') {
-//           console.log(error.code);
-//           process.exit(1);
+
+//       let hashtagCount = hashtag.reduce((acc, hashtag) => {
+//         let existingHashtag = acc.find(obj => obj.text === hashtag);
+//         if (existingHashtag) {
+//             existingHashtag.value++;
+//         } else {
+//             acc.push({text: hashtag, value: 1});
+//         }
+//         return acc;
+//       }, []);
+//       hashtagCount.sort((a, b) => b.value - a.value);
+//       socket.emit('hashtag', hashtagCount[0])
+
+//       count++;
+//     } catch (error) {
+//       if (data.detail === "This stream is currently at the maximum allowed connection limit.") {
+//         console.log(data.detail)
+//         // process.exit(1)
 //       } else {
-//           setTimeout(() => {
-//               console.warn("A connection error occurred. Reconnecting...")
-//               streamConnect(++retryAttempt);
-//           }, 2 ** retryAttempt)
-//       }
-//   });
-//   return stream;
+//         // Keep alive signal received. Do nothing.
+//     }
+//     }
+//   })
+//   return stream
 // }
 
 
-// // (async () => {
-// //     let currentRules;
-// //     try {
-// //         currentRules = await getAllRules();
-// //         await deleteAllRules(currentRules);
-// //         await setRules();
-// //     } catch (e) {
-// //         console.error(e);
-// //         process.exit(1);
-// //     }
-// //     streamConnect(0);
-// // })();
+// async function start(io) {
+//   console.log("=>>>>>>>>>>>>>")
+//   let currentRules
+//   try {
+//     currentRules = await getRules()
+//     await deleteRules(currentRules)
+//     await setRules()
+//   } catch (error) {
+//     console.error(error)
+//     process.exit(1)
+//   }
+//   const filteredStream = streamTweets(io)
+//   let timeout = 0
+//   filteredStream.on('timeout', () => {
+//     // Reconnect on error
+//     console.warn('A connection error occurred. Reconnecting…')
+//     setTimeout(() => {
+//       timeout++
+//       start(io)
+//     }, 2 ** timeout)
+//     start(io)
+//   })
+// }
 
-// io.on('connection', (socket) => {
-//   console.log(`New client connected ${socket.id}`);
+// io.on('connection', async (socket) => {
+//   console.log('Client connected...')
+
+//   socket.on('message', (data) => {
+//     if(data){
+//       console.log("start:")
+//       rules = [{value:data}]
+//       // rules = [{value:`${data} -is:retweet`}]
+//       start(io)
+//     }
+//   });
 
 //   socket.on('disconnect', () => {
-//     console.log('Client disconnected');
+//     console.log('disconnected')
+//     if (stream) {
+//       stream.destroy()
+//     }
 //   });
+// })
 
-//   socket.on('message', (message) => {
-//     console.log(`Message received: ${message}`);
-//     // io.emit('message', message);
-//   });
-// });
-
-
-server.listen(PORT,()=> {
-  console.log(`Listening on ${PORT}`)
-})
+// server.listen(PORT,()=> {
+//   console.log(`Listening on ${PORT}`)
+// })
 
 
